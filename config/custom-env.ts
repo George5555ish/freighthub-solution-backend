29dcc1b73477431802acaa4074711c208b3ab7ce
George5555ish <mbajiakugeorge@yahoo.com>
2022-03-17 21:14:57 +0100

    Heroku

--- config/custom-env.ts
+++ config/custom-env.ts
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
     publicKey: "PUBLIC_KEY",
     privateKey: "PRIVATE_KEY",
     MONGO_DB_URL: "MONGO_DB_URL"
--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,3 @@
-export default {
+module.exports = {
     dbUri: "mongodb://localhost:27017/freighthub-shipment-db"
 }
